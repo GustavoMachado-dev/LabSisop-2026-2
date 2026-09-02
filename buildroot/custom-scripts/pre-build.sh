98a48c4111d4ef7d2a40285da30eff9e30be16fd
Mvfm-25 <mvfm.filho@hotmail.com>
2026-09-02 20:59:09 -0300

    Implementação de Systeminfo.

--- buildroot/custom-scripts/pre-build.sh
+++ buildroot/custom-scripts/pre-build.sh
@@ -1,4 +1,15 @@
 #!/bin/sh
 
-cp $BASE_DIR/../custom-scripts/S41network-config $BASE_DIR/target/etc/init.d
+CUSTOM_DIR=$BASE_DIR/../custom-scripts
+
+# Configuracao de rede do tutorial 1.2
+cp $CUSTOM_DIR/S41network-config $BASE_DIR/target/etc/init.d
 chmod +x $BASE_DIR/target/etc/init.d/S41network-config
+
+# Aplicacao systeminfo (servidor REST na porta 8080)
+cp $CUSTOM_DIR/systeminfo $BASE_DIR/target/usr/bin/systeminfo
+chmod +x $BASE_DIR/target/usr/bin/systeminfo
+
+# Inicializacao automatica do systeminfo no boot
+cp $CUSTOM_DIR/S99systeminfo $BASE_DIR/target/etc/init.d
+chmod +x $BASE_DIR/target/etc/init.d/S99systeminfo
